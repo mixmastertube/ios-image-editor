bd62e2027cb499b4dc314cf0484cf1cc81079cba
Pau Ballada <pau@pegpeg.net>
2015-04-06 20:23:55 +0200

    add useCircularImage at .h

--- ImageEditor/HFImageEditorViewController.h
+++ ImageEditor/HFImageEditorViewController.h
@@ -21,6 +21,7 @@ typedef void(^HFImageEditorDoneCallback)(UIImage *image, BOOL canceled);
 @property(nonatomic,assign) CGFloat minimumScale;
 @property(nonatomic,assign) CGFloat maximumScale;
 
+@property(nonatomic,assign) BOOL useCircularImage;
 @property(nonatomic,assign) BOOL panEnabled;
 @property(nonatomic,assign) BOOL rotateEnabled;
 @property(nonatomic,assign) BOOL scaleEnabled;
